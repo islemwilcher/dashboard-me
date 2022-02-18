d5fafbdf9645ecc7679ef43f0ed35f96d65a3c3e
islemwilcher <islemadobe6@gmail.com>
2022-02-18 03:32:12 +0100

    making the manage styles

--- src/pages/manage/styles.js
+++ src/pages/manage/styles.js
@@ -1,5 +1,9 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
-
+    grid: {
+        display: 'flex',
+        flexDirection: 'column',
+        marginTop: '20px',
+    }
 }))
