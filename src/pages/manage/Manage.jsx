6c1ea31aaad9636965043fd270cc8b2f7cb6dcbf
islemwilcher <islemadobe6@gmail.com>
2022-02-18 03:28:28 +0100

    creating the manage page

--- src/pages/manage/Manage.jsx
+++ src/pages/manage/Manage.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+
+import { useState } from 'react'
+import { Grow, Container, Grid } from '@material-ui/core'
+
+import Form from './form/Form'
+import Projects from '../projects/Projects'
+
+import useStyles from './styles'
 
 const Manage = () => {
+
+    const [currentId, setCurrentId] = useState(0)
+    const classes = useStyles()
+    
     return (
-        <>
-            
-        </>
+        <Grow in>
+            <Container>
+                <Grid className={classes.grid} container align='center' spacing={3}>
+                    <Grid item >
+                        <Form currentId={currentId} setCurrentId={setCurrentId} />
+                    </Grid>
+                    <Grid item >
+                        <Projects setCurrentId={setCurrentId} />
+                    </Grid>
+                </Grid>
+        </Container>
+        </Grow>
     )
 };
 
